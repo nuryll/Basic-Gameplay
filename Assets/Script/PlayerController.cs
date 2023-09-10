9709fb89dbc5b18647e4caec89af2dd4e873a031
Nur Yl <52735279+nuryll@users.noreply.github.com>
2023-09-10 12:36:13 +0200

    Add Vertical player movement

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public float minZ = -1f;         // Minimum Z position the player can move to
+    public float maxZ = 10f;          // Maximum Z position the player can move to
+
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
@@ -28,6 +31,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        // Calculate movement based on player input (typically W and S keys or up and down arrow keys)
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        // Compute the new position
+        float newZ = transform.position.z + moveZ;
+
+        // Ensure the new position is within the specified range
+        newZ = Mathf.Clamp(newZ, minZ, maxZ);
+
+        // Update player position
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -6,7 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
+    public float spawnRangeY = 5f; // Range in Y where animals can spawn
     private float spawnPosZ = 20;
+
+    private float nextSpawnTime = 0f;
+    public float spawnCooldown = 3f;
+    public float animalSpeed = 5f;
 
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
@@ -20,8 +25,25 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > nextSpawnTime)
+        {
+            SpawnAnimal();
+            nextSpawnTime = Time.time + spawnCooldown;
+        }
+
+    }
+
+    void SpawnAnimal()
+    {
+        // Randomly choose whether to spawn from the left (-1) or the right (1)
+        int direction = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        Vector3 spawnPosition = new Vector3(-direction * 10, 5.0f, Random.Range(-spawnRangeY, spawnRangeY));
+        GameObject animal = Instantiate(animalPrefabs[Random.Range(0, animalPrefabs.Length)], spawnPosition, Quaternion.identity);
+
+        // Move the animal towards the opposite side
+        animal.transform.LookAt(new Vector3(direction * 10, 5.0f, spawnPosition.z));
        
-        
     }
 
     void SpawnRandomAnimal()
